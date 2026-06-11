81a659c57a74113bebd773ff1cc184a472957be7
SoftwareEngineerK1 <199867455+SoftwareEngineerK1@users.noreply.github.com>
2026-06-11 17:12:02 -0400

    Delete edu-portals directory

--- edu-portals/myPAW/supabase/policies.sql
+++
@@ -1,9 +0,0 @@
-create policy "profiles_select_own"
-on public.mypaw_profiles
-for select
-using (auth.uid() = auth_user_id);
-
-create policy "profiles_update_own"
-on public.mypaw_profiles
-for update
-using (auth.uid() = auth_user_id);
